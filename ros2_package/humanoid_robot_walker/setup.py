14fc869e7d33664db5a10531f6f42d687532e9c8
AshwinderPalSingh <Asingh47_be23@thapar.edu>
2025-08-26 18:12:54 +0530

    Create setup.py

---
+++ ros2_package/humanoid_robot_walker/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import os
+from glob import glob
+
+package_name = 'humanoid_robot_walker'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your.email@example.com',
+    description='2D Humanoid robot walker with keyboard and coordinate control',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'robot_controller = humanoid_robot_walker.robot_controller:main',
+        ],
+    },
+)
